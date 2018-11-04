6b70499641c2fe306f320a26a494c28f744f87dc
aburk3 <aburk3@gmail.com>
2018-11-04 14:06:42 -0600

    Added menu and goodbye method to cli class

--- lib/top_startups/cli.rb
+++ lib/top_startups/cli.rb
@@ -3,12 +3,13 @@
 class TopStartups::CLI
 
   def call
-    puts "Today's Top Startups:"
     list_startups
-    
+    menu
+    goodbye
   end
 
   def list_startups
+    puts "Today's Top Startups:"
     puts <<~DOC
       1. Medium - United States
       2. Airbnb - United States
@@ -16,5 +17,27 @@ class TopStartups::CLI
       4. Uber - United States
       5. Canva - Australia
     DOC
+  end
+
+  def menu
+    input = nil
+    while input != "exit"
+      puts "Enter the number of the startup you would like more info on, type 'list' to see the startups, or you can type exit:"
+      input = gets.strip.downcase
+      case input
+      when "1"
+        puts "More info on startup 1..."
+      when "2"
+        puts "More info on startup 2..."
+      when "list"
+        list_startups
+      else
+        puts "Did not recognize, please type list or exit."
+      end
+    end
+  end
+
+  def goodbye
+    puts "Come back later to checkout new rankings!"
   end
 end
