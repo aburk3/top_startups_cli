3851f3c04584429d381f35fc898bc35e92abbc73
aburk3 <aburk3@gmail.com>
2018-11-04 16:09:10 -0600

    Added scraping method and added nokogiri and pry as requirements

--- lib/top_startups.rb
+++ lib/top_startups.rb
@@ -1,3 +1,7 @@
+require 'open-uri'
+require 'nokogiri'
+require 'pry'
+
 require_relative "./top_startups/version"
 require_relative './top_startups/cli'
 require_relative './top_startups/startups'
--- lib/top_startups/startups.rb
+++ lib/top_startups/startups.rb
@@ -1,24 +1,21 @@
 class TopStartups::Startups
   attr_accessor :name, :location, :rank, :srscore, :funding, :url
+
   def self.today
+    self.scrape_startups
+  end
 
-    startup_1 = self.new
-    startup_1.name = "Medium"
-    startup_1.location = "United States"
-    startup_1.rank = "1"
-    startup_1.srscore = "90,889"
-    startup_1.funding = "$50,000,000"
-    startup_1.url = "https://www.startupranking.com/medium"
+  def self.scrape_startups
+    startups = []
 
-    startup_2 = self.new
-    startup_2.name = "Airbnb"
-    startup_2.location = "United States"
-    startup_2.rank = "2"
-    startup_2.srscore = "90,653"
-    startup_2.funding = "$447,800,000"
-    startup_2.url = "https://www.startupranking.com/airbnb"
+    startups << self.scrape_ranking
 
-    [startup_1, startup_2]
+    startups
+  end
+
+  def self.scrape_ranking
+    doc = Nokogiri::HTML(open(https://www.startupranking.com/top))
+    
   end
 
 end
