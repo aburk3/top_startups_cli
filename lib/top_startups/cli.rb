d203285424afb32551dfe2ed11e795abe9dc6d82
aburk3 <aburk3@gmail.com>
2018-11-04 20:59:44 -0600

    Attempting to fix the scrape for ranking.

--- lib/top_startups/cli.rb
+++ lib/top_startups/cli.rb
@@ -19,26 +19,30 @@ class TopStartups::CLI
   def menu
     input = nil
     while input != "exit"
+      puts " "
       puts "Enter the number of the startup you would like more info on, type 'list' to see the startups, or you can type exit:"
       input = gets.strip.downcase
 
-      if input.to_i > 0
+      if input.to_i > 0 && input.to_i <= 5
         the_startup = @startups[input.to_i-1]
         puts "---------------------------------------------"
-        puts "#{the_startup.name} - #{the_startup.location}"
+        puts "#{the_startup.name}"
         puts <<~DOC
+          Location: #{the_startup.location}"
           Rank: #{the_startup.rank}
           SR Score: #{the_startup.srscore}
         DOC
       elsif input == "list"
+        puts "---------------------------------------------"
         list_startups
-      else
+      elsif input != "exit"
         puts "Did not recognize the input, please type list or exit."
       end
     end
   end
 
   def goodbye
+    puts " "
     puts "Come back later to checkout new rankings!"
   end
 end
--- lib/top_startups/startups.rb
+++ lib/top_startups/startups.rb
@@ -8,14 +8,31 @@ class TopStartups::Startups
   def self.scrape_startups
     startups = []
 
-    startups << self.scrape_ranking
-
+    startups << self.scrape_ranking_one
+    startups << self.scrape_ranking_two
+    startups << self.scrape_ranking_three
+    startups << self.scrape_ranking_four
+    startups << self.scrape_ranking_five
     startups
   end
 
-  def self.scrape_ranking
-    doc = Nokogiri::HTML(open("https://www.startupranking.com/top"))
+  # def self.scrape_ranking_one
+  #   4.times do |i|
+  #     doc = Nokogiri::HTML(open("https://www.startupranking.com/top"))
+  #     startups = self.new
+  #     startups.name = doc.search(".ranks td.tleft div.name")[i].children.text
+  #     startups.location = doc.search(".ranks td .f32").children.css("img")[i].attribute("title").value
+  #     startups.rank = doc.search(".ranks td .f32").children.css("div")[i].children.text
+  #     startups.srscore = doc.search(".ranks td.tright.sr-score")[i].children.text.strip
+  #     startups
+  #   end
+  # end
 
+
+  ##<Nokogiri::XML::Element:0x3fcc9649ccb0 name="td" children=[#<Nokogiri::XML::Text:0x3fcc96499600 "99">
+  def self.scrape_ranking_one
+    doc = Nokogiri::HTML(open("https://www.startupranking.com/top"))
+    binding.pry
     startups = self.new
     startups.name = doc.search(".ranks td.tleft div.name")[0].children.text
     startups.location = doc.search(".ranks td .f32").children.css("img")[0].attribute("title").value
@@ -23,7 +40,48 @@ class TopStartups::Startups
     startups.srscore = doc.search(".ranks td.tright.sr-score")[0].children.text.strip
     startups
   end
-end
 
-# doc.search(".ranks td .f32").children[3]
-# => #(Element:0x3fddc4d1fdd0 { name = "div", attributes = [ #(Attr:0x3fddc4d1fd6c { name = "class", value = "country-rank" })], children = [ #(Text "1")] })
+  def self.scrape_ranking_two
+    doc = Nokogiri::HTML(open("https://www.startupranking.com/top"))
+
+    startups = self.new
+    startups.name = doc.search(".ranks td.tleft div.name")[1].children.text
+    startups.location = doc.search(".ranks td .f32").children.css("img")[1].attribute("title").value
+    startups.rank = doc.search(".ranks td .f32").children.css("div")[1].children.text
+    startups.srscore = doc.search(".ranks td.tright.sr-score")[1].children.text.strip
+    startups
+  end
+
+  def self.scrape_ranking_three
+    doc = Nokogiri::HTML(open("https://www.startupranking.com/top"))
+
+    startups = self.new
+    startups.name = doc.search(".ranks td.tleft div.name")[2].children.text
+    startups.location = doc.search(".ranks td .f32").children.css("img")[2].attribute("title").value
+    startups.rank = doc.search(".ranks td .f32").children.css("div")[2].children.text
+    startups.srscore = doc.search(".ranks td.tright.sr-score")[2].children.text.strip
+    startups
+  end
+
+  def self.scrape_ranking_four
+    doc = Nokogiri::HTML(open("https://www.startupranking.com/top"))
+
+    startups = self.new
+    startups.name = doc.search(".ranks td.tleft div.name")[3].children.text
+    startups.location = doc.search(".ranks td .f32").children.css("img")[3].attribute("title").value
+    startups.rank = doc.search(".ranks td .f32").children.css("div")[3].children.text
+    startups.srscore = doc.search(".ranks td.tright.sr-score")[3].children.text.strip
+    startups
+  end
+
+  def self.scrape_ranking_five
+    doc = Nokogiri::HTML(open("https://www.startupranking.com/top"))
+
+    startups = self.new
+    startups.name = doc.search(".ranks td.tleft div.name")[4].children.text
+    startups.location = doc.search(".ranks td .f32").children.css("img")[4].attribute("title").value
+    startups.rank = doc.search(".ranks td .f32").children.css("div")[4].children.text
+    startups.srscore = doc.search(".ranks td.tright.sr-score")[4].children.text.strip
+    startups
+  end
+end
